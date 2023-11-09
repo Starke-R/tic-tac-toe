de2820b9b834ca5bc17b6ce213fc6ac006c64464
starkeOnline <starke.robin@outlook.com>
2023-11-09 13:57:20 +0100

    Added grid and divided each section to a numbered id

--- game.js
+++ game.js
@@ -1,5 +1,50 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let spaceOne = canvas.getContext("2d");
+let spaceTwo = canvas.getContext("2d");
+let spaceThree = canvas.getContext("2d");
+let spaceFour = canvas.getContext("2d");
+let spaceFive = canvas.getContext("2d");
+let spaceSix = canvas.getContext("2d");
+let spaceSeven = canvas.getContext("2d");
+let spaceEight = canvas.getContext("2d");
+let spaceNine = canvas.getContext("2d");
 
-canvas.width = 500;
-canvas.height = 500;
+
+canvas.width = 600;
+canvas.height = 600;
+
+//Vertical lines
+ctx.strokeRect(200, 25, 1, 550);
+ctx.strokeRect(400, 25, 1, 550);
+
+//Horizontal lines
+ctx.strokeRect(25, 200, 550, 1);
+ctx.strokeRect(25, 400, 550, 1);
+
+spaceOne.fillStyle = "green";
+spaceOne.fillRect(0, 0, 200, 200);
+
+spaceTwo.fillStyle = "blue";
+spaceTwo.fillRect(200, 0, 200, 200);
+
+spaceThree.fillStyle = "green";
+spaceThree.fillRect(400, 0, 200, 200);
+
+spaceFour.fillStyle = "blue";
+spaceFour.fillRect(0, 200, 200, 200);
+
+spaceFive.fillStyle = "green";
+spaceOne.fillRect(200, 200, 200, 200);
+
+spaceSix.fillStyle = "blue";
+spaceSix.fillRect(400, 200, 200, 200);
+
+spaceSeven.fillStyle = "green";
+spaceSeven.fillRect(0, 400, 200, 200);
+
+spaceEight.fillStyle = "blue";
+spaceEight.fillRect(200, 400, 200, 200);
+
+spaceNine.fillStyle = "green";
+spaceNine.fillRect(400, 400, 200, 200);
